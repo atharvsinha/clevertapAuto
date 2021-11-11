57afe313b589937240dcefd6d8e9c3b0b9e3058f
Atharv Sinha <atharvsinha@Atharvs-MacBook-Air.local>
2021-11-11 16:53:14 +0530

    Clevertap Automated

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = process.env.PORT || 80;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let data = [];
+let display = [];
+
+app.post("/upload", (req, res) => {
+  data.push(req.body);
+  display.push(`${req.body.username} is pushed!`);
+});
+app.get("/", (req, res) => {
+  res.send(display);
+});
+console.log(port);
+console.log(Date.now());
+app.listen(port);
